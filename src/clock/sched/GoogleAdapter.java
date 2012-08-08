e754c36efd9872e39cbe0229ff8f9ebfd5b0aca0
Ron Zionpour <ronzionp@gmail.com>
2012-08-08 19:24:19 +0300

    Added GoogleAdapter and some new Exceptions

--- src/clock/sched/GoogleAdapter.java
+++ src/clock/sched/GoogleAdapter.java
@@ -1,7 +1,12 @@
 package clock.sched;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import clock.db.Event;
 import clock.outsources.GoogleTrafficHandler;
+import clock.outsources.GoogleTrafficHandler.TrafficData;
 import clock.outsources.GoogleWeatherHandler;
 
 public class GoogleAdapter {
@@ -11,17 +16,55 @@ public class GoogleAdapter {
 	
 	private static String lastOrigin;
 	private static Event lastEvent;
+	private static float lastDistanceToEventLocation = 0;
 	
-	public static long getTravelTimeToEvent(Event nextEvent) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static TrafficData getTrafficData(Context context, Event event) throws Exception
+	{
+		String origin = getOrigin(context);
+		TrafficData trafficData = gTrafficHandler.calculateTrafficInfo(origin, event.getLocation());
+		return trafficData;
+	}
+	
+	public static long getTravelTimeToEvent(Context context, Event event) throws Exception
+	{
+		String origin = getOrigin(context);
+		TrafficData trafficData = gTrafficHandler.calculateTrafficInfo(origin, event.getLocation());
+		return trafficData.getDuration();
 	}
 
-	public static float getDistanceToEventLocation(Event event) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static float getDistanceToEventLocation(Context context, Event event) throws Exception
+	{
+		String origin = getOrigin(context);
+		if (isCacheDetails(origin, event))
+		{
+			return lastDistanceToEventLocation;
+		}
+		
+		TrafficData trafficData = gTrafficHandler.calculateTrafficInfo(origin, event.getLocation());
+		return trafficData.getDistance();
 	}
-	
+
+	private static boolean isCacheDetails(String origin, Event event) {
+		if (origin.equals(lastOrigin) && event.equals(lastEvent)
+				&& lastDistanceToEventLocation != 0)
+		{
+			return true;
+		}
+		
+		return false;
+			
+	}
+
+	private static String getOrigin(Context context) 
+	{
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		String provider = lm.getBestProvider(criteria, true);
+		Location location = lm.getLastKnownLocation(provider);
+		return location.getLongitude() + "," + location.getLatitude();
+	}
+
 	public static boolean isLegalAddress(String address)
 	{
 		// TODO Auto-generated method stub
@@ -32,5 +75,7 @@ public class GoogleAdapter {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
+
 
 }
