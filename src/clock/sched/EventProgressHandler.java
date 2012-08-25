a4a8c3a93e3966daa300d83f33f59e6e1f04550c
Ron Zionpour <ronzionp@gmail.com>
2012-08-25 14:00:11 +0300

    Added critical msg when user is late

--- src/clock/sched/EventProgressHandler.java
+++ src/clock/sched/EventProgressHandler.java
@@ -1,27 +1,14 @@
 package clock.sched;
 
-import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.AlarmManager;
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
-import android.os.IBinder;
-import android.provider.AlarmClock;
-import android.sax.StartElementListener;
-import android.text.GetChars;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import clock.db.Event;
 import clock.outsources.GoogleTrafficHandler.TrafficData;
 
@@ -50,7 +37,7 @@ public class EventProgressHandler{
 		// Remind user to go out if needed
 		if (timesLeftToGoOut <= GO_OUT_REMINDER_TIME)
 		{
-			final String msg = "Time to go out in " + TimeUnit.MILLISECONDS.toMinutes(timesLeftToGoOut) +  " Minutes";			
+			String msg = "Time to go out in " + TimeUnit.MILLISECONDS.toMinutes(timesLeftToGoOut) +  " Minutes";			
 			notifyUser(context, msg);
 		}
 		
@@ -66,30 +53,37 @@ public class EventProgressHandler{
 		Log.d("PROGRESS", "Handling event progress from location handler");
 		loadDetailsFromEvent(event);
 		
-		try
+		if (userHasBeenWakedUp)	//Case that user has already been waked up and moved 100 meters atleast
 		{
-			TrafficData trafficData = GoogleAdapter.getTrafficData(context, event, location);
-			long diffTime = TimeUnit.SECONDS.toMillis(trafficData.getDuration()) - event.getTimesLeftToEvent();
-			
-//			if (diffTime > 0)
-//			{
-//				criticalMsg(context, event);
-//			}
-//			else if (diffTime <= GO_OUT_REMINDER_TIME)
-//			{
-//				notifyUser(context, diffTime, event);
-//			}
-			
+			//TODO: notify the learning machine.
 		}
-		catch (Exception e) {
-			Log.e("PROGRESS", "Error while trying to check if user is late: " + e.getMessage());
+		else
+		{
+			try
+			{
+				TrafficData trafficData = GoogleAdapter.getTrafficData(context, event, location);
+				long diffTime = TimeUnit.SECONDS.toMillis(trafficData.getDuration()) - event.getTimesLeftToEvent();
+				
+				if (diffTime > 0)
+				{
+					String msg = "You're going to be late for event - " + event.toString();
+					criticalMsg(context, msg);
+				}
+				else if (diffTime <= GO_OUT_REMINDER_TIME)
+				{
+					String msg = "Time to go out in " + TimeUnit.MILLISECONDS.toMinutes(diffTime) +  " Minutes";
+					notifyUser(context, msg);
+				}
+				
+			}
+			catch (Exception e) {
+				Log.e("PROGRESS", "Error while trying to check if user is late: " + e.getMessage());
+			}
 		}
 		
 		saveDetailsToEvent(event);
 		
 	}
-
-
 
 	private static boolean isItTimeToWakeUp(long timesLeftToGoOut, long arrangeTime) 
 	{
@@ -118,6 +112,7 @@ public class EventProgressHandler{
 				+ " Minutes";
 		
 		//TODO: can't start activity from context
+		//TODO: set location handler to notify after 100 meters movement
 		
 		userHasBeenWakedUp = true;
 		Log.d("PROGRESS", "User has been waked up with message: " + msg);
@@ -131,23 +126,49 @@ public class EventProgressHandler{
 		Log.d("PROGRESS", "Notifying user with: " + msg);
 		
 		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		int icon = R.drawable.icon;
-		long when = System.currentTimeMillis();		//For showing the notification now
-
-		Notification notification = new Notification(icon, msg, when);
-		notification.defaults |= Notification.DEFAULT_SOUND;
-		String title = "AppDate";
-		Intent intent = new Intent(context, EventProgressHandler.class);
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 		
-		notification.setLatestEventInfo(context, title, msg, pendingIntent);
+		Notification notification = getBasicNotification(context, msg);
+		notification.defaults |= Notification.DEFAULT_SOUND;
 		
 		notificationManager.notify(NOTIFICATION_ID, notification);
 		Log.d("PROGRESS", "User has been notified");
 		
 		userHasBeenNotified = true;
 	}
+
+
+	private static void criticalMsg(Context context, String msg) 
+	{
+		if (userHasBeenNotified)
+			return;
+		
+		Log.d("PROGRESS", "Critical message to user with: " + msg);
+		
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		Notification notification = getBasicNotification(context, msg);
+		notification.defaults |= Notification.DEFAULT_SOUND;
+		notification.flags |= Notification.FLAG_INSISTENT;		//This will cause annoying notification
+				
+		notificationManager.notify(CRITICAL_ID, notification);
+		Log.d("PROGRESS", "User has been critical messaged");
+		
+		userHasBeenNotified = true;		
+	}
 	
+	private static Notification getBasicNotification(Context context, String msg) {
+		int icon = R.drawable.icon;
+		long when = System.currentTimeMillis();		//For showing the notification now
+		Notification notification = new Notification(icon, msg, when);
+		String title = "AppDate";
+		Intent intent = new Intent(context, EventProgressHandler.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+		
+		notification.setLatestEventInfo(context, title, msg, pendingIntent);
+		
+		return notification;
+	}
+
 	private static void loadDetailsFromEvent(Event event) {
 		userHasBeenNotified = event.isUserHasBeenNotified();
 		userHasBeenWakedUp = event.isUserHasBeenWakedUp();
